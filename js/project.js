24c50f40593573600d4027199bcc71b0ef57e4f0
chimchimchimmy <Stephanielipe1@gmail.com>
2024-11-20 18:42:40 -0800

    Proceed

--- js/project.js
+++ js/project.js
@@ -1,34 +1,21 @@
-
-
-
-// BELOW FOR WELCOME PAGE
-
-document.getElementById('myClick');
+// Get the element with the ID 'myClick'
+var clickBox = document.getElementById('myClick');
 
 // Get the OK button
-
 var okButton = document.getElementById('okBtn');
 
 // Get the main content area
-
 var mainContent = document.getElementById('mainContent');
 
 // When the page loads, display the box
-
 window.onload = function() {
-
-  click.style.display = 'flex'; // Show the box
-
-}
+  clickBox.style.display = 'flex'; // Show the box
+};
 
 // When the user clicks the "OK" button, hide the box and show the content
-
 okButton.onclick = function() {
-
-  click.style.display = 'none'; // Hide the box
-
+  clickBox.style.display = 'none'; // Hide the box
   mainContent.style.display = 'block'; // Show the main content
-
-}
+};
 
 
